abac5a5409139f413b61faab1a67f2efec74334e
classtak <classtak@gmail.com>
2018-01-02 14:11:29 +0900

    routes.rb 완료

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ RESTful이란, 주소창(url)을 통해서 자원(리소스)과 행위(HTTP Verb
   get '/posts/new' =>
   post '/posts'=>
   # Read
-  get '/posts/:id'  =>
+  get '/posts/:id' =>
   # Update
   get '/posts/:id/edit' =>
   put '/posts/:id' =>
@@ -31,6 +31,12 @@ RESTful이란, 주소창(url)을 통해서 자원(리소스)과 행위(HTTP Verb
 
 ### 2. scaffolding.. 편하게 CRUD
 
-### 3. 파일업로드(내일...ㅠ_ㅠ)
+### 3. 파일업로드(내일..? 넘어갈수도..)
+
+  `gem carrierwave`
+
+### 3-1. 사진 크기 조절하여 저장
+
+  `gem mini_magick`
 
 ### 4. 인스타처럼 꾸미기(카드형 배치)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  get '/posts' => 'posts#index'
+  # Create
+  get '/posts/new' => 'posts#new'
+  post '/posts'=> 'posts#create'
+  # Read
+  get '/posts/:id' => 'posts#show'
+  # Update
+  get '/posts/:id/edit' => 'posts#edit'
+  put '/posts/:id' => 'posts#update'
+  # Delete
+  delete '/posts/:id' => 'posts#delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
